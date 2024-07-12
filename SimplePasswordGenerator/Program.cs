218a8bed922375e95526dd3a0877ed7e33ca7441
samuel-williams-ed <70590831+samuel-williams-ed@users.noreply.github.com>
2024-07-12 23:38:29 +0100

    Add project files.

---
+++ SimplePasswordGenerator/Program.cs
@@ -0,0 +1,119 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PasswordGenerator;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace passwordApp
+{
+
+    class Program
+    {
+
+        class Option
+        {
+            public string? num;
+            public string? text;
+        }
+
+        // create Single Thread Apartment function, 
+        // required for System.Windows.Forms (copying to Clipboard)
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            // Ensure the application runs in STA mode (required for clipboard operations)
+            // Though text and UI is not relevant for cmdln app - these settings ensure compatibility 
+            // and rendering consistency for applicationas relying on this app 
+            // (ie., the clipboard) when using some modern and/or international fonts
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+            // One source of truth for password generation options
+            Option[] options = new Option[] {
+                new() {num = "0", text="default, 16 letter"},
+                new() {num = "1", text="short"}, 
+                new() {num = "2", text="4-number PIN"},
+                new() {num = "3", text="6-number PIN"},
+                new() {num = "4", text="alphanumeric"},
+                new() {num = "5", text="long"},
+                new() {num = "6", text="long-alphanumeric"}
+            };
+
+
+            // variables (with standard values, to be overwritten)
+            var userSelectedText = "Standard 16 alphanumeric letters, upper and lower case, with symbols.";
+            var pwd = new Password();
+
+            Console.WriteLine("\nWelcome to password generator:");
+            string? userInput = "0";
+
+            while ( userInput != null && !userInput.Equals("q", StringComparison.OrdinalIgnoreCase))
+            {
+                generatePassword();
+                Console.WriteLine("\nHit enter to generate another password. \n Press 'q' to quit");
+                userInput = Console.ReadLine();
+            };
+            
+            void generatePassword()
+            {
+
+                // get user input
+                Console.WriteLine("Here, what kind of password would you like?");
+                foreach (Option opt in options) 
+                { 
+                    Console.WriteLine($"{opt.num}: {opt.text}"); 
+                }
+                Console.WriteLine("---");
+                var userSelection = Console.ReadLine();
+
+
+                // logic to handle user selection
+                pwd = new Password();
+                userSelectedText = "Standard 16 alphanumeric letters, upper and lower case, with symbols.";
+                if (userSelection == options[1].num)
+                {
+                    pwd = new Password(6);
+                    userSelectedText = options[1].text;
+                }
+                if (userSelection == options[2].num)
+                {
+                    pwd = (Password) new Password(4).IncludeNumeric();
+                    userSelectedText = options[2].text;
+                }
+                if (userSelection == options[3].num)
+                {
+                    pwd = (Password) new Password(6).IncludeNumeric();
+                    userSelectedText = options[3].text;
+                }
+                if (userSelection == options[4].num)
+                {
+                    pwd = (Password) new Password().IncludeNumeric().IncludeLowercase().IncludeUppercase();
+                    userSelectedText = options[4].text;
+                }
+                if (userSelection == options[5].num)
+                {
+                    pwd = (Password) new Password(32);
+                    userSelectedText = options[5].text;
+                }
+                if (userSelection == options[6].num)
+                {
+                    pwd = (Password) new Password(32).IncludeNumeric().IncludeLowercase().IncludeUppercase();
+                    userSelectedText = options[6].text;
+                }
+
+                // By default, all characters available for use and a length of 16
+                // Will return a random password with the default settings 
+                var password = pwd.Next();
+                Console.WriteLine($"Generating password: {userSelectedText}");
+
+                Clipboard.SetText(password);
+                Console.WriteLine(password.ToString());
+                Console.WriteLine($"(This has been copied to your clipboard!)");
+            }
+        }
+    }
+}
+
+
---
+++ SimplePasswordGenerator/README.txt
@@ -0,0 +1,23 @@
+// A simple password generator in the command line
+// - allows the user to select from a variety of passwords
+// - uses .Net Standard Library package "PasswordGenerator" by prjseal (see licence at bottom)
+
+// To run the application
+`dotnet run`
+
+// to publish a standalone executable application - 
+`dotnet publish -c Release -r win-x64 --self-contained`
+
+// you can then find the application in
+`//DIRECTORY_TO_APP/bin/Release/net8.0/win-x64/publish`
+
+
+// MIT Licence 
+
+// Copyright (c) <2022> <prjseal>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
